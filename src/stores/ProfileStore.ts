cc50e8d7ff31db13b12773052b1cd704873c7ca1
Giacomo Rebonato <grebonato@zendesk.com>
2016-08-25 11:44:42 +0100

    Removed semicolons

--- src/stores/ProfileStore.ts
+++ src/stores/ProfileStore.ts
@@ -119,10 +119,10 @@ class ProfileStore extends BaseStore {
 
 		uploadTask.on('state_changed', (snapshot) => {
 			var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-			console.log('Upload is ' + progress + '% done');
+			console.log('Upload is ' + progress + '% done')
 			switch (snapshot.state) {
 				case firebase.storage.TaskState.PAUSED: // or 'paused'
-					console.log('Upload is paused');
+					console.log('Upload is paused')
 					break
 				case firebase.storage.TaskState.RUNNING: // or 'running'
 					console.log('Upload is running')
